672bf97ef9928e999a6ff7ed0c5f9025e021e824
Abdur Rahim <arahimcse@gmail.com>
2023-09-13 19:34:58 +0600

    vue in project

--- module_01/cl2/control-structure/index.php
+++ module_01/cl2/control-structure/index.php
@@ -1,33 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php control structure</title>
 </head>
+
 <body>
-    <h1>Welcome to our Control Structure</h1>
-    <ul>
-        <li><a href="./in.php">Introduction</a></li>
-        <li><a href="./if.php">IF</a></li>
-        <li><a href="./else.php">Else</a></li>
-        <li><a href="./elseif.php">elseif/else if</a></li>
-        <li><a href="./alsyn.php">Alternative syntax for control structures</a></li>
-        <li><a href="./whil.php">while</a></li>
-        <li><a href="./dowhile.php">do-while</a></li>
-        <li><a href="./for.php">for</a></li>
-        <li><a href="./foreach.php">Foreach</a></li>
-        <li><a href="./break.php">Break </a></li>
-        <li><a href="./continue.php">Continue</a></li>
-        <li><a href="./switch.php">Switch</a></li>
-        <li><a href="https://www.php.net/manual/en/control-structures.match.php">Match</a></li>
-        <li><a href="https://www.php.net/manual/en/control-structures.declare.php">Declare</a></li>
-        <li><a href="https://www.php.net/manual/en/function.return.php">Return</a></li>
-        <li><a href="https://www.php.net/manual/en/function.require.php">Require</a></li>
-        <li><a href="https://www.php.net/manual/en/function.include.php">Include</a></li>
-        <li><a href="https://www.php.net/manual/en/function.require-once.php">Require Once</a></li>
-        <li><a href="https://www.php.net/manual/en/function.include-once.php">Include Once</a></li>
-        <li><a href="https://www.php.net/manual/en/control-structures.goto.php">Goto</a></li>
-    </ul>
+    <div id="app">
+        {{ message }}
+        <button @click="increment">Count is: {{ count }}</button>
+    </div>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="./asset/main.js"></script>
 </body>
+
 </html>
