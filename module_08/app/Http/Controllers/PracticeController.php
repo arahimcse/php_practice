9fb7e5a00d641886500de9155523734797cdb901
Abdur Rahim <arahimcse@gmail.com>
2023-12-16 22:24:24 +0600

    laravel basic

---
+++ module_08/app/Http/Controllers/PracticeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PracticeController extends Controller
+{
+    function show(Request $request, $name)
+    {
+
+    }
+}
